9b758358dbc51c23207102b32f690275ee19adc9
Andy Hunt <andy.l.hunt85@gmail.com>
2018-08-15 23:26:40 -0400

    Working pegs

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -4,24 +4,25 @@ class Game
 
     def initialize(player1)
         @player1 = player1
-        @colors = [:yellow, :blue, :red, :green, :purple, :black]
+        @colors = ["yellow", "blue", "red", "green", "purple", "black"]
         @rounds = 1
         @peg_rounds = []
         @guess_rounds = []
     end
 
     def begin_game
-        @code = @colors.collect { |color| @colors.sample }.slice(0,4) 
+        @code = @colors.shuffle.slice(0,4) 
         play
     end
 
     def play
-        puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        puts "The code has six colors: Red, Blue, Yellow, Green, Orange, and Black."
-        puts "You have 10 rounds to guess the code or the computer wins!"
-        puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        puts
+        puts "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        puts "The code has six colors: Red, Blue, Yellow, Green, Purple, and Black."
+        puts "There are no duplicates. You have 10 rounds to guess the code or the computer wins!"
+        puts "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        puts
         loop do
-            print @code
             get_player_choices
             add_pegs
             board
@@ -31,7 +32,7 @@ class Game
 
     def board 
         @guess_rounds.each_with_index do |el, index|
-            puts "ROUND #{index+1}: #{@guess_rounds[index].join(", ")}  |  #{@peg_rounds[index].join(", ")}"
+            puts "ROUND #{index+1}: #{@guess_rounds[index].join(", ").ljust(30)}  |  #{@peg_rounds[index].join(", ")}"
         end
         puts
     end
@@ -40,10 +41,10 @@ class Game
         puts "Round #{@rounds}. #{@player1.name}, choose a color. Type 'colors' to get the list."
         @player_choices = []
         while @player_choices.count < 4
-            guess = gets.chomp.downcase.strip.to_sym
+            guess = gets.chomp.downcase.strip
             is_valid?(guess)
         end
-        @guess_rounds.push(@player_choices)
+        @guess_rounds << @player_choices
     end
 
     def is_valid?(guess)
@@ -58,14 +59,20 @@ class Game
 
     def add_pegs
         pegs = []
+        code_temp = @code.clone
+        print code_temp
         @player_choices.each_with_index do |el, index|
-            if el == @code[index]
+            if el == code_temp[index]
                 pegs << "black"
-            elsif @code.include?(el)
+                index = code_temp.index(el)
+                code_temp[index] = "!"
+            elsif code_temp.include?(el)
                 pegs << "white"
+                index = code_temp.index(el)
+                code_temp[index] = "!"
             end
         end
-        pegs.sort
+        pegs.sort!
         code_broken?(pegs)
         @peg_rounds.push(pegs)
         puts
@@ -81,7 +88,7 @@ class Game
     end
 
     def play_again? 
-        puts "Would you like to play again? Yes to continue, No to exit. "
+        puts "Would you like to play again? Type 'yes' to continue, 'no' to exit. "
         next_game = gets.chomp.strip.downcase
         next_game == "yes" ? reset_game : exit
     end
